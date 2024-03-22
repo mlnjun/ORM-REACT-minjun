c40742dda3fbaa38f82bbdaeb9d410d3628c7e3c
mlnjun <rhalswns098@gmail.com>
2024-03-22 16:22:17 +0900

    tailwind 실습1

--- NEXT.js/next-first-app/app/regist/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Regist() {
-  return <div>회원 가입 페이지</div>
-}
---
+++ NEXT.js/nextjs-sample-app/app/login/page.js
@@ -0,0 +1,154 @@
+'use client'
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Login() {
+  const router = useRouter()
+
+  // 유저 로그인 정보 상태
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  // 유저 로그인 정보 상태 바인딩 핸들러
+  const onUserChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  // 유저 로그인 submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const postLogin = async () => {
+      const res = await fetch('http://localhost:3005/api/member/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      })
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+
+      console.log(res)
+
+      const result = await res.json()
+
+      console.log(result)
+
+      if (result.code == '200') {
+        router.push('/')
+      } else if (result.code == '500') {
+        return <div>서버 오류</div>
+      }
+    }
+
+    postLogin()
+  }
+
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">로그인</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                이메일
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={user.email}
+                  onChange={onUserChange}
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  암호
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={user.password}
+                  onChange={onUserChange}
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="text-right">
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
